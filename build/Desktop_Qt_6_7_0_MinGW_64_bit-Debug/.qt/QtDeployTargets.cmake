3569f4488f0ef186e85fa84ece307ea65fd86cda
amar jadhav <jadhavamar3020@gmail.com>
2024-04-29 10:47:35 +0530

    Initial commit

---
+++ build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_CarDashBoard_FILE D:/newqtproject/CarDashBoard-20240429T051435Z-001/CarDashBoard/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/CarDashBoard.exe)
+set(__QT_DEPLOY_TARGET_CarDashBoard_RUNTIME_DLLS D:/qt/6.7.0/mingw_64/bin/Qt6Sql.dll;D:/qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;D:/qt/6.7.0/mingw_64/bin/Qt6Gui.dll;D:/qt/6.7.0/mingw_64/bin/Qt6Sql.dll;D:/qt/6.7.0/mingw_64/bin/Qt6Core.dll)
